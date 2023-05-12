967736f9be28250874435fa3fa3ce5f1db832897
Shilna P <pshilsrihanat1@gmail.com>
2023-05-12 22:59:32 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+const PORT=process.env.PORT ||3000
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+  });
+  
+  io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);    });
+  });
+  
+server.listen(PORT, () => {
+    console.log(`listening on ${PORT}`);
+  });
+
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
